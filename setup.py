b39f385f9f751a0e9308f19ccc01d9002ea7ce70
hashimmg <55830640+hashimmg@users.noreply.github.com>
2023-11-06 15:14:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
 
+"""Setup JAX-CFD."""
+import setuptools
+
+base_requires = ['jax','jax_cfd', 'numpy', 'scipy', 'tree-math']
+data_requires = ['matplotlib', 'seaborn', 'Pillow', 'xarray']
+ml_requires = ['dm-haiku', 'einops', 'gin-config']
+tests_requires = ['absl-py', 'pytest', 'pytest-xdist', 'scikit-image']
+
+setuptools.setup(
+    name='jax_IB',
+    version='0.1.0',
+    license='Apache 2.0',
+    author='Alhashim',
+    author_email='mohammed.mga122@gmail.com',
+    install_requires=base_requires,
+    extras_require={
+        'data': data_requires,
+        'ml': ml_requires,
+        'tests': tests_requires,
+        'complete': data_requires + ml_requires + tests_requires,
+    },
+    url='https://github.com/',
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+)
