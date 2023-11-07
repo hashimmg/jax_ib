09e06bf5ab7bb3713b4c3d67e74517eb245ae501
hashimmg <55830640+hashimmg@users.noreply.github.com>
2023-11-07 14:48:59 +0300

    Create __init__.py

---
+++ Jax_IB/base/__init__.py
@@ -0,0 +1,9 @@
+import Jax_IB.base.IBM_Force
+import Jax_IB.base.boundaries
+import Jax_IB.base.convolution_functions
+import Jax_IB.base.equations
+import Jax_IB.base.grids
+import Jax_IB.base.particle_class
+import Jax_IB.base.particle_motion
+import Jax_IB.base.pressure
+import Jax_IB.base.time_stepping
