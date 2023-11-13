da94725ee03700ec2b772c8743a2c349059f0f61
hashimmg <55830640+hashimmg@users.noreply.github.com>
2023-11-13 22:01:31 +0300

    Update __init__.py

--- jax_ib/base/__init__.py
+++ jax_ib/base/__init__.py
@@ -1,1 +1,16 @@
+import jax_ib.base.IBM_Force
+import jax_ib.base.boundaries
+import jax_ib.base.convolution_functions
+import jax_ib.base.equations
+import jax_ib.base.grids
+import jax_ib.base.particle_class
+import jax_ib.base.particle_motion
+import jax_ib.base.pressure
+import jax_ib.base.time_stepping
+
+import jax_ib.base.advection
+import jax_ib.base.interpolation
+import jax_ib.base.diffusion
+import jax_ib.base.finite_differences
+import jax_ib.base.kinematics
 
